2faee69aa27c61bea2265c2b8b58f5609b393e4e
William Duckitt <wdduckitt@sun.ac.za>
2023-04-17 11:13:49 +0200

    cleaned up naming

--- opcuaEpicsBridge/bridge.py
+++ opcuaEpicsBridge/bridge.py
@@ -136,58 +136,59 @@ if __name__ == "__main__":
         opcuaClient["epicsToOpcuaNames"]={}
         
         for record in db.values():
-            epicsType=record.fields["DTYP"]
-           
-            epicsPvName=str(record.name)
-           
-            epicsType=str(record.rtyp).upper()
-            opcuaType=str(record.fields["OPCUA_TYPE"])
-            epicsPvs[epicsPvName]={}
-            epicsPvs[epicsPvName]["initialized"]=False
+            DTYP=str(record.fields["DTYP"]).upper()
+            if "OPCUA" in DTYP:
                 
-            if "AI" in epicsType:
-                opcuaName=str(record.fields["INP"])
-                fields={}
-                for field in record.fields:
-                    upper=str(field).upper()
-                    if upper in ["DESC","EGU","HOPR","LOPR","PREC"]:
-                        fields[upper]=record.fields[field]
-                
-                epicsPvs[epicsPvName]["pv"]=builder.aIn(epicsPvName,**fields)
-                epicsPvs[epicsPvName]["epicsType"]="AI"
-                
+                epicsPvName=str(record.name)
+            
+                epicsType=str(record.rtyp).upper()
+                opcuaType=(DTYP.split("_"))[1].capitalize()
+                epicsPvs[epicsPvName]={}
+                epicsPvs[epicsPvName]["initialized"]=False
+                    
+                if "AI" in epicsType:
+                    opcuaName=str(record.fields["INP"])
+                    fields={}
+                    for field in record.fields:
+                        upper=str(field).upper()
+                        if upper in ["DESC","EGU","HOPR","LOPR","PREC"]:
+                            fields[upper]=record.fields[field]
+                    
+                    epicsPvs[epicsPvName]["pv"]=builder.aIn(epicsPvName,**fields)
+                    epicsPvs[epicsPvName]["epicsType"]="AI"
+                    
 
-            if "AO" in epicsType:
-                opcuaName=str(record.fields["OUT"])
-                fields={}
-                for field in record.fields:
-                    upper=str(field).upper()
-                    if upper in ["DESC","EGU","HOPR","LOPR","PREC"]:
-                        fields[upper]=record.fields[field]
-                epicsPvs[epicsPvName]["pv"]=builder.aOut(epicsPvName,on_update=partial(on_epics_pv_update,opcuaClientName=name,epicsPvName=epicsPvName,epicsType=epicsType,opcuaName=opcuaName,opcuaType=opcuaType,opcuaClients=clients,debug=debug,**fields),**fields)
-                epicsPvs[epicsPvName]["epicsType"]="AO"
+                if "AO" in epicsType:
+                    opcuaName=str(record.fields["OUT"])
+                    fields={}
+                    for field in record.fields:
+                        upper=str(field).upper()
+                        if upper in ["DESC","EGU","HOPR","LOPR","PREC"]:
+                            fields[upper]=record.fields[field]
+                    epicsPvs[epicsPvName]["pv"]=builder.aOut(epicsPvName,on_update=partial(on_epics_pv_update,opcuaClientName=name,epicsPvName=epicsPvName,epicsType=epicsType,opcuaName=opcuaName,opcuaType=opcuaType,opcuaClients=clients,debug=debug,**fields),**fields)
+                    epicsPvs[epicsPvName]["epicsType"]="AO"
 
-            elif "BO" in epicsType:
-                opcuaName=str(record.fields["OUT"])
-                fields={}
-                for field in record.fields:
-                    upper=str(field).upper()
-                    if upper in ["ZNAM","ONAM","DESC","EGU","HOPR","LOPR","PREC"]:
-                        fields[upper]=record.fields[field]
-                epicsPvs[epicsPvName]["pv"]=builder.boolOut(epicsPvName,on_update=partial(on_epics_pv_update,opcuaClientName=name,epicsPvName=epicsPvName,epicsType=epicsType,opcuaName=opcuaName,opcuaType=opcuaType,opcuaClients=clients,**fields),**fields)
-                epicsPvs[epicsPvName]["epicsType"]="BO"
-            elif "BI" in epicsType:
-                opcuaName=str(record.fields["INP"])
-                fields={}
-                for field in record.fields:
-                    upper=str(field).upper()
-                    if upper in ["ZNAM","ONAM","DESC","EGU","HOPR","LOPR","PREC"]:
-                        fields[upper]=record.fields[field]
-                epicsPvs[epicsPvName]["pv"]=builder.boolIn(epicsPvName,**fields)
-                epicsPvs[epicsPvName]["epicsType"]="BI"
-            opcuaClient["opcuaToEpicsNames"][str(opcuaName)]=str(epicsPvName)
-            opcuaClient["epicsToOpcuaNames"][str(epicsPvName)]=str(opcuaName)    
-            opcuaClient["sub"].subscribe_data_change(opcuaClient["client"].get_node(opcuaName))
+                elif "BO" in epicsType:
+                    opcuaName=str(record.fields["OUT"])
+                    fields={}
+                    for field in record.fields:
+                        upper=str(field).upper()
+                        if upper in ["ZNAM","ONAM","DESC","EGU","HOPR","LOPR","PREC"]:
+                            fields[upper]=record.fields[field]
+                    epicsPvs[epicsPvName]["pv"]=builder.boolOut(epicsPvName,on_update=partial(on_epics_pv_update,opcuaClientName=name,epicsPvName=epicsPvName,epicsType=epicsType,opcuaName=opcuaName,opcuaType=opcuaType,opcuaClients=clients,**fields),**fields)
+                    epicsPvs[epicsPvName]["epicsType"]="BO"
+                elif "BI" in epicsType:
+                    opcuaName=str(record.fields["INP"])
+                    fields={}
+                    for field in record.fields:
+                        upper=str(field).upper()
+                        if upper in ["ZNAM","ONAM","DESC","EGU","HOPR","LOPR","PREC"]:
+                            fields[upper]=record.fields[field]
+                    epicsPvs[epicsPvName]["pv"]=builder.boolIn(epicsPvName,**fields)
+                    epicsPvs[epicsPvName]["epicsType"]="BI"
+                opcuaClient["opcuaToEpicsNames"][str(opcuaName)]=str(epicsPvName)
+                opcuaClient["epicsToOpcuaNames"][str(epicsPvName)]=str(opcuaName)    
+                opcuaClient["sub"].subscribe_data_change(opcuaClient["client"].get_node(opcuaName))
         # except Exception as e:
         #     print("e1",e)
     except Exception as e:
